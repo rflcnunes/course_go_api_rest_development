9aa1bfed4ece33e2953c91e4078094e6fbc5d6fd
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-31 16:20:59 -0300

    added package to controllers

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,17 +1,14 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/rflcnunes/course_go_api_rest_development/internal/controllers"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
-}
-
 func handleRequest() {
-	http.HandleFunc("/", Home)
+	http.HandleFunc("/", controllers.Home)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
