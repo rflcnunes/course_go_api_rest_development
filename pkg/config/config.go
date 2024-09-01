0793372d310abd33807ceb411bc1e4f58b60ac41
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-09-01 00:47:26 -0300

    update config with database and update dockerfile

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,10 +3,16 @@ package config
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/rflcnunes/course_go_api_rest_development/internal/controllers"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
 
 func handleRequest() {
 	r := mux.NewRouter()
@@ -17,6 +23,28 @@ func handleRequest() {
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
+func setupEnv() {
+	log.Println("INFO: Loading .env file")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("ERROR: loading .env file", err)
+	}
+}
+
+func setupDB() {
+	log.Println("INFO: Connecting to database")
+	dsn := "host=" + os.Getenv("POSTGRES_HOST") + " user=" + os.Getenv("POSTGRES_USER") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " dbname=" + os.Getenv("POSTGRES_DB") + " sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("ERROR: Failed to connect to database", err)
+	}
+
+	DB = db
+
+	log.Println("INFO: Connected to database")
+}
+
 func Setup() {
+	setupEnv()
+	setupDB()
 	handleRequest()
 }
